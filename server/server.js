8b3f3ddea86d5206debac461b4cfa81ee128e590
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-29 01:52:36 +0800

    Add jwt auth

--- server/server.js
+++ server/server.js
@@ -1,25 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
-const { chats } = require('./data/data');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 dotenv.config();
 connectDB();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is Running');
 });
 
-app.get('/api/chat', (req, res) => {
-  res.send(chats);
-});
-
-app.get('/api/chat/:id', (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 

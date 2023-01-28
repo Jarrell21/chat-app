4cb3be931b8e55d9f8c0906a675ea07fa5376c00
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-29 01:51:13 +0800

    Update

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', true);
-
 const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', true);
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,

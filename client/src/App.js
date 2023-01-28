f675ccf6260e63ee7abf94e4a6f95bb901bfcbfe
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-28 23:06:59 +0800

    Move router from index.js to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,31 @@ import {
   ChakraProvider,
   theme,
 } from '@chakra-ui/react';
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './routes/Homepage';
+import ChatPage from './routes/ChatPage';
+import ErrorPage from './routes/ErrorPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/chat",
+    element: <ChatPage />,
+  },
+])
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <div className='App'>
+        <RouterProvider router={router}/>
+      </div>
     </ChakraProvider>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,15 @@
 import { ColorModeScript } from '@chakra-ui/react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import Homepage from './routes/Homepage';
-import ChatPage from './routes/ChatPage';
-import ErrorPage from './routes/ErrorPage';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/chat",
-    element: <ChatPage />,
-  },
-])
 
 root.render(
   <StrictMode>
     <ColorModeScript />
-    <RouterProvider router={router}/>
     <App />
   </StrictMode>
 );

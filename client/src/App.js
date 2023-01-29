69c7335812bb8cb1a5a247543d19a5d9aac417a8
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-30 00:27:08 +0800

    Change router syntax

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  theme,
-} from '@chakra-ui/react';
 import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import { Route, Routes } from 'react-router-dom';
+import StartingPage from './pages/StartingPage';
 import ChatPage from './pages/ChatPage';
-import ErrorPage from './pages/ErrorPage';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LoginPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/chat",
-    element: <ChatPage />,
-  },
-])
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
       <div className='App'>
-        <RouterProvider router={router}/>
+        <Routes>
+          <Route path="/" element={<StartingPage />} exact />
+          <Route path="/chats" element={<ChatPage />} />
+        </Routes>
       </div>
-    </ChakraProvider>
   );
 }
 

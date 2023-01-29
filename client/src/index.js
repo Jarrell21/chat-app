b2368abeadaaaf650ad433d0bf2d245e969f94e2
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-30 00:27:25 +0800

    Update index

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,24 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import {
+  ChakraProvider,
+  theme,
+} from '@chakra-ui/react';
+import ChatProvider from './context/ChatProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 
 root.render(
-  <StrictMode>
-    <ColorModeScript />
-    <App />
-  </StrictMode>
+  <ChakraProvider theme={theme}>
+    <BrowserRouter>
+      <ChatProvider>
+        <ColorModeScript />
+        <App />
+      </ChatProvider>
+    </BrowserRouter>
+  </ChakraProvider>
 );

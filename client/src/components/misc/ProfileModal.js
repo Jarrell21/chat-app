773a4c00bab3247bb890f34613e0eb0415f053bc
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-30 00:29:16 +0800

    Create profile model

---
+++ client/src/components/misc/ProfileModal.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    IconButton,
+    Button,
+    Flex,
+    Image,
+    Text,
+  } from '@chakra-ui/react'
+import {useDisclosure} from '@chakra-ui/hooks'
+import { ViewIcon } from '@chakra-ui/icons'
+
+const ProfileModal = ({user, children}) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <div>
+        {children ? (<span onClick={onOpen}>{children}</span>) : (
+            <IconButton 
+                display={{base: 'flex'}}
+                icon={<ViewIcon />}
+                onClick={onOpen}
+            />
+        )} 
+        <Modal isOpen={isOpen} onClose={onClose} size='lg' isCentered>
+        <ModalOverlay />
+        <ModalContent>
+
+          <ModalHeader>
+            <Flex
+                fontFamily={'Work sans'}
+                fontSize='3xl'
+                justify={'center'}
+            >
+             Profile Information
+            </Flex>
+          </ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody>
+            <Flex gap={'20px'}>
+                <Image 
+                    boxSize={'150px'}
+                    src={user.pic}
+                    alt={user.name}
+                />
+                <Flex
+                    direction={'column'}
+                    justify='center'
+                    fontFamily={'Work sans'}
+                    fontSize='1.3rem'
+                    fontWeight={'800'}
+                >
+                    <Text>
+                        Name: {user.name}
+                    </Text>
+                    <Text>
+                        Email: {user.email}
+                    </Text>
+                </Flex>
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter>
+          </ModalFooter>
+
+        </ModalContent>
+      </Modal>
+    </div>
+  )
+}
+
+export default ProfileModal

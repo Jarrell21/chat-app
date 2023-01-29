d35f7763e03ce4c08cea06c961ec5b9b55b34b39
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-30 07:15:05 +0800

    Refactor code

--- server/controllers/chatControllers.js
+++ server/controllers/chatControllers.js
@@ -6,19 +6,19 @@ const accessChat = asyncHandler(async (req, res) => {
   const { userId } = req.body;
 
   if (!userId) {
-    console.log('userId param not sent with request');
+    console.log('UserId param not sent with request');
     return res.sendStatus(400);
   }
 
   let isChat = await Chat.find({
-    isGroupchat: false,
+    isGroupChat: false,
     $and: [
       { users: { $elemMatch: { $eq: req.user._id } } },
       { users: { $elemMatch: { $eq: userId } } },
     ],
   })
     .populate('users', '-password')
-    .populate('lastestMessage');
+    .populate('latestMessage');
 
   isChat = await User.populate(isChat, {
     path: 'latestMessage.sender',
@@ -30,19 +30,17 @@ const accessChat = asyncHandler(async (req, res) => {
   } else {
     const chatData = {
       chatName: 'sender',
-      isGroupchat: false,
+      isGroupChat: false,
       users: [req.user._id, userId],
     };
 
     try {
       const createdChat = await Chat.create(chatData);
-
-      const fullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+      const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
         'users',
         '-password'
       );
-
-      res.status(200).json(fullChat);
+      res.status(200).json(FullChat);
     } catch (error) {
       res.status(400);
       throw new Error(error.message);
@@ -56,14 +54,13 @@ const fetchChats = asyncHandler(async (req, res) => {
       .populate('users', '-password')
       .populate('groupAdmin', '-password')
       .populate('latestMessage')
-      .sort({ updatedAt: 1 })
-      .then(async (result) => {
-        result = await User.populate(result, {
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
           path: 'latestMessage.sender',
           select: 'name pic email',
         });
-
-        res.status(200).send(result);
+        res.status(200).send(results);
       });
   } catch (error) {
     res.status(400);
@@ -73,7 +70,7 @@ const fetchChats = asyncHandler(async (req, res) => {
 
 const createGroupChat = asyncHandler(async (req, res) => {
   if (!req.body.users || !req.body.name) {
-    return res.status(400).send({ message: 'Fill all the fields' });
+    return res.status(400).send({ message: 'Please Fill all the feilds' });
   }
 
   const users = JSON.parse(req.body.users);
@@ -81,7 +78,7 @@ const createGroupChat = asyncHandler(async (req, res) => {
   if (users.length < 2) {
     return res
       .status(400)
-      .send('More than 2 users are required to create a group chat');
+      .send('More than 2 users are required to form a group chat');
   }
 
   users.push(req.user);
@@ -128,29 +125,6 @@ const renameGroup = asyncHandler(async (req, res) => {
   }
 });
 
-const addToGroup = asyncHandler(async (req, res) => {
-  const { chatId, userId } = req.body;
-
-  const added = await Chat.findByIdAndUpdate(
-    chatId,
-    {
-      $push: { users: userId },
-    },
-    {
-      new: true,
-    }
-  )
-    .populate('users', '-password')
-    .populate('groupAdmin', '-password');
-
-  if (!added) {
-    res.status(404);
-    throw new Error('Chat Not Found');
-  } else {
-    res.json(added);
-  }
-});
-
 const removeFromGroup = asyncHandler(async (req, res) => {
   const { chatId, userId } = req.body;
 
@@ -174,11 +148,34 @@ const removeFromGroup = asyncHandler(async (req, res) => {
   }
 });
 
+const addToGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate('users', '-password')
+    .populate('groupAdmin', '-password');
+
+  if (!added) {
+    res.status(404);
+    throw new Error('Chat Not Found');
+  } else {
+    res.json(added);
+  }
+});
+
 module.exports = {
   accessChat,
   fetchChats,
   createGroupChat,
   renameGroup,
-  removeFromGroup,
   addToGroup,
+  removeFromGroup,
 };
